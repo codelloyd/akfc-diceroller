6e6a92356bf8db860b8ab7ca67f4820d77d0f3da
Lloyd Balce <lloydvbalce@gmail.com>
2019-08-10 15:46:12 +0800

    Add another dice

--- app/src/main/java/com/codelloyd/akfc/diceroller/MainActivity.kt
+++ app/src/main/java/com/codelloyd/akfc/diceroller/MainActivity.kt
@@ -7,13 +7,15 @@ import android.widget.ImageView
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
-  lateinit var diceImage: ImageView
+  lateinit var diceImage1: ImageView
+  lateinit var diceImage2: ImageView
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    diceImage = findViewById(R.id.dice_image)
+    diceImage1 = findViewById(R.id.dice_image_1)
+    diceImage2 = findViewById(R.id.dice_image_2)
 
     val rollButton: Button = findViewById(R.id.roll_button)
     rollButton.setOnClickListener { rollDice() }
@@ -25,9 +27,17 @@ class MainActivity : AppCompatActivity() {
   }
 
   private fun rollDice() {
-    val randomInt = Random().nextInt(6) + 1
+    diceImage1.setImageResource(getRandomDiceImage())
+    diceImage2.setImageResource(getRandomDiceImage())
+  }
 
-    val drawableResource = when (randomInt) {
+  private fun reset() {
+    diceImage1.setImageResource(R.drawable.empty_dice)
+    diceImage2.setImageResource(R.drawable.empty_dice)
+  }
+
+  private fun getRandomDiceImage(): Int {
+    return when (Random().nextInt(6) + 1) {
       1 -> R.drawable.dice_1
       2 -> R.drawable.dice_2
       3 -> R.drawable.dice_3
@@ -35,10 +45,5 @@ class MainActivity : AppCompatActivity() {
       5 -> R.drawable.dice_5
       else -> R.drawable.dice_6
     }
-    diceImage.setImageResource(drawableResource)
-  }
-
-  private fun reset() {
-    diceImage.setImageResource(R.drawable.empty_dice)
   }
 }
