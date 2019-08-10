96ca74bcb074f6b574de2518f93cbb07a5caf3db
Lloyd Balce <lloydvbalce@gmail.com>
2019-08-10 15:36:56 +0800

    Use image resources and adjust compatibility

--- app/src/main/java/com/codelloyd/akfc/diceroller/MainActivity.kt
+++ app/src/main/java/com/codelloyd/akfc/diceroller/MainActivity.kt
@@ -3,14 +3,17 @@ package com.codelloyd.akfc.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+  lateinit var diceImage: ImageView
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    diceImage = findViewById(R.id.dice_image)
 
     val rollButton: Button = findViewById(R.id.roll_button)
     rollButton.setOnClickListener { rollDice() }
@@ -24,13 +27,18 @@ class MainActivity : AppCompatActivity() {
   private fun rollDice() {
     val randomInt = Random().nextInt(6) + 1
 
-    val resultText: TextView = findViewById(R.id.result_text)
-    resultText.text = randomInt.toString()
+    val drawableResource = when (randomInt) {
+      1 -> R.drawable.dice_1
+      2 -> R.drawable.dice_2
+      3 -> R.drawable.dice_3
+      4 -> R.drawable.dice_4
+      5 -> R.drawable.dice_5
+      else -> R.drawable.dice_6
+    }
+    diceImage.setImageResource(drawableResource)
   }
 
   private fun reset() {
-    val resultText: TextView = findViewById(R.id.result_text)
-    val resetValue = 0
-    resultText.text = resetValue.toString()
+    diceImage.setImageResource(R.drawable.empty_dice)
   }
 }
